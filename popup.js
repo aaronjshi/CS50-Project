b02792e75a11a1c7264cff548480d2ccf30f25a3
aaronjshi <aaron_shi@college.harvard.edu>
2016-12-01 23:53:10 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -50,20 +50,86 @@ window.onload = function() {
 			.done(function(json) {
 				var state = json['location']['state'];
 				var city = json['location']['city'];
-				//var img_src = json['current_observation']['icon_url'];
+				var img_src = json['current_observation']['icon_url'];
 				var temp_f = json['current_observation']['temp_f'];
 				var chancePrecipitation = json['forecast']['txt_forecast']['forecastday'][0]['pop'];
 				var feelsLike = json['current_observation']['feelslike_f'];
+				var humidity = json['current_observation']['relative_humidity'];
+				var wind = json['current_observation']['wind_gust_mph'];
 
-				//document.getElementById('img_src').src = img_src;
+				$('#img_src').attr("src", img_src);
 				document.getElementById('city,state').innerHTML = city + ", " + state;
 				document.getElementById('temperature').innerHTML = "Temperature (F): " + temp_f + "\xB0, feels like " + feelsLike + "\xB0";
-				document.getElementById('CoP').innerHTML = "Chance of precipitation: " + chancePrecipitation + "%";
+				document.getElementById('humidity').innerHTML = "Humidity: " + humidity;
+				document.getElementById('CoP').innerHTML = "Precipitation: " + chancePrecipitation + "%";
+				document.getElementById('wind').innerHTML = "Wind: " + wind + " mph";
 
+
+				var top;
+				var bot;
+				var jacket;
+				var str;
+				var precip;
+
+				// top layer
+				if (feelsLike >= 60) {
+					top = "T-shirt";
+				}
+				else if (feelsLike >= 50) {
+					top = "long sleeve";
+				}
+				else {
+					top = "sweater/fleece pullover";
+				}
+				// bottom layer
+				if (feelsLike >= 65) {
+					bot = "shorts";
+				}
+				else if (feelsLike >= 30) {
+					bot = "long pants";
+				}
+				else {
+					bot = "long pants, baselayer";
+				}
+				// jacket
+				if (feelsLike >= 60) {
+					jacket = "";
+				}
+				else if (feelsLike >= 45) {
+					jacket = "light jacket";
+				}
+				else if (feelsLike >= 30) {
+					jacket = "heavy jacket";
+				}
+				else {
+					jacket = "snow jacket";
+				}
+
+				if (feelsLike >= 90) {
+					str = "really hot";
+				}
+				else if (feelsLike >= 70) {
+					str = "hot";
+				}
+				else if (feelsLike >= 60) {
+					str = "moderate";
+				}
+				else if (feelsLike >= 50) {
+					str = "chilly";
+				}
+				else if (feelsLike >= 30) {
+					str = "cold";
+				}
+				else {
+					str = "really cold";
+				}
+
+				document.getElementById('string').innerHTML = "It's " + str + "! Wear a " + top + ", " + bot + ", and a " + jacket + ".";
 			})
 	 
 			.fail(function( xhr, status, errorThrown ) {
 			    alert( "Sorry, there was a problem!" );
+			    console.log("Error");
 			})
 		})
 	};
