a33dddc0324c563e98a657120977210389d7817b
mchiang123 <michellechiang@college.harvard.edu>
2016-12-01 20:00:18 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,3 @@
-// add listener for when icon is clicked?
-
-// get user's location using chrome api
 // var geolocation_API_key = "AIzaSyBaHJ6Z-Xf7XeZ9wCcwBXXNADq6OPh8BOE";
 
 window.onload = function() {
@@ -16,14 +13,13 @@ window.onload = function() {
 	}
 	var geoSuccess = function(position) {
 	startPos = position;
-
 	lat = startPos.coords.latitude;
 	long = startPos.coords.longitude;
 
-	document.getElementById('startLat').innerHTML = lat;
-	document.getElementById('startLon').innerHTML = long;
-
+	// call getWeather function (see below)
+	getWeather(lat, long);
 	};
+	
 	var geoError = function(error) {
 	console.log('Error occurred. Error code: ' + error.code);
 	// error.code can be:
@@ -32,9 +28,11 @@ window.onload = function() {
 	//   2: position unavailable (error response from location provider)
 	//   3: timed out
 	};
+	
 	navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 
 	// get weather conditions in current location
+	function getWeather(lat, long) {
 	var API_key = "8e89558cffdf7c1a";
 	var url = "http://api.wunderground.com/api/" + API_key + "/geolookup/conditions/q/" + lat + "," + long + ".json";
 	document.getElementById('demo').innerHTML = url;
@@ -43,31 +41,31 @@ window.onload = function() {
 		console.log("test");
 		$.ajax({
 	 
-	    // The URL for the request
-	    url: url,
-	    //"http://api.wunderground.com/api/8e89558cffdf7c1a/geolookup/conditions/q/IA/Cedar_Rapids.json",
+		// The URL for the request
+	    	url: url,
+	    	//"http://api.wunderground.com/api/8e89558cffdf7c1a/geolookup/conditions/q/IA/Cedar_Rapids.json",
 	 
-	    // Whether this is a POST or GET request
-	    type: "GET",
+	    	// Whether this is a POST or GET request
+	    	type: "GET",
 	 
-	    // The type of data we expect back
-	    dataType : "json",
-	})
-	  // Code to run if the request succeeds (is done);
-	  // The response is passed to the function
-	  .done(function(json) {
-	  	var location = json['location']['city'];
+	    	// The type of data we expect back
+	    	dataType : "json",
+		})
+		// Code to run if the request succeeds (is done);
+		// The response is passed to the function
+		.done(function(json) {
+		var location = json['location']['city'];
 			var temp_f = json['current_observation']['temp_f'];
 			alert("Current temperature in " + location + " is: " + temp_f);
 
 			document.getElementById('demo').innerHTML = temp_f;
-	  })
+		})
 
-	  .fail(function( xhr, status, errorThrown ) {
-	    alert( "Sorry, there was a problem!" );
+		.fail(function( xhr, status, errorThrown ) {
+		    alert( "Sorry, there was a problem!" );
 		})
 	})
-
+	};
 };
 
 
